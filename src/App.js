84a53b7ff142e1872443a045cb3733e612270ff9
Jamesson Sales de Sena <jsdesena@interaxa.com>
2019-10-17 16:24:05 -0300

    Salvando/removendo informações no firebase

--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ export default class App extends Component {
   constructor(props){
     super(props);
     this.state = {
+      tokenInput:'',
+      nomeInput:'',
+      idadeInput:'',
       token: 'Carregando...',
       nome: '',
       idade: ''
     };
-
+    this.cadastrar = this.cadastrar.bind(this);
 
     const firebaseConfig = {
       apiKey: "AIzaSyAEpchbR257dc_1dWl5FRa8VHw5lYoB_ds",
@@ -39,9 +42,32 @@ export default class App extends Component {
     });
   }
   
+  cadastrar(e){
+   //Salvando/Editando -> firebase.database().ref('token').set(this.state.tokenInput);
+   //Salvando uma coluna nova-> firebase.database().ref('usuarios').child(1).child('Cargo').set(this.state.tokenInput);
+   //Removendo -> firebase.database().ref('usuarios').child(1).child('Cargo').remove();
+
+    let usuarios = firebase.database().ref('usuarios');
+    let chave = usuarios.push().key;
+
+    usuarios.child(chave).set({
+      Nome:this.state.nomeInput,
+      Idade: this.state.idadeInput
+    });
+
+   e.preventDefault();//Para não atualizar a pagina quando clicar
+  }
+
   render(){
       return (
         <div>
+            <form onSubmit={this.cadastrar}>
+              <input type="text" value={this.state.tokenInput} onChange={(e)=>this.setState({tokenInput: e.target.value})}/><br/>
+              <input type="text" value={this.state.nomeInput} onChange={(e)=>this.setState({nomeInput: e.target.value})}/><br/>
+              <input type="text" value={this.state.idadeInput} onChange={(e)=>this.setState({idadeInput: e.target.value})}/><br/>
+             
+              <button type="submit">cadastrar</button>
+            </form>
             <h1>Token: {this.state.token}</h1>
             <h1>Nome: {this.state.nome}</h1>
             <h1>Idade: {this.state.idade}</h1>
